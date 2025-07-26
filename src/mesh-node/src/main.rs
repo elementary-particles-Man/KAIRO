b533b2a9f29f2a1a3ace78e14c84b8442876e58d
素粒子 <elementary.particles.japan@gmail.com>
2025-07-27 07:38:36 +0900

    Add log option to mesh_node

--- src/mesh-node/src/main.rs
+++ src/mesh-node/src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+#[derive(Parser)]
+struct Args {
+    /// Log file path
+    #[arg(long)]
+    log: Option<String>,
+}
+
 fn main() {
+    let args = Args::parse();
+    let mut logfile = args
+        .log
+        .as_ref()
+        .and_then(|p| OpenOptions::new().create(true).append(true).open(p).ok());
+
     println!("Mesh node started.");
+    if let Some(file) = logfile.as_mut() {
+        let _ = writeln!(file, "Mesh node started.");
+    }
 }
