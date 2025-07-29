f31981015eb81042be67604cf1fb6d7cb563e7b0
素粒子 <elementary.particles.japan@gmail.com>
2025-07-29 11:31:47 +0900

    Expand AiTcpPacket struct

--- src/kairo-lib/packet.rs
+++ src/kairo-lib/packet.rs
@@ -5,6 +5,8 @@ pub struct AiTcpPacket {
     pub source: String,
     pub destination: String,
     pub payload: String,
+    pub source_public_key: String,
+    pub destination_p_address: String,
+    pub signature: String
 }
 
-// 既存の他のPacket構造体があればこの下に追記
