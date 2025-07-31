f082d42328065d0e39558fdbb4ab0e16bce59da3
素粒子 <elementary.particles.japan@gmail.com>
2025-07-31 10:48:25 +0900

    \"Fix: Cargo.toml configurations and merge conflicts\"

--- src/bot/main.rs
+++ src/bot/main.rs
@@ -1,5 +1,3 @@
-//! src/bot/main.rs
-
 use simple_logger;
 use log::*;
 use warp::Filter;
@@ -9,12 +7,19 @@ use api::status::status_route;
 
 #[tokio::main]
 async fn main() {
-    simple_logger::SimpleLogger::new().with_level(log::LevelFilter::Debug).init().unwrap();
-    info!("KAIROBOT: Logger initialized successfully. Starting warp server...");
+    // ログ初期化
+    simple_logger::SimpleLogger::new()
+        .with_level(log::LevelFilter::Debug)
+        .init()
+        .unwrap();
+    info!("KAIROBOT: Logger initialized successfully.");
 
+    // ルーティング構築
     let health_check = warp::path("health").map(|| warp::reply::json(&"OK"));
     let routes = status_route().or(health_check);
 
+    // HTTPサーバ起動
+    info!("KAIROBOT: Starting warp HTTP server on 127.0.0.1:4040...");
     warp::serve(routes)
         .run(([127, 0, 0, 1], 4040))
         .await;
